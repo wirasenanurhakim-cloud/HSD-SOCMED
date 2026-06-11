c1224e611017fe998e3a3c7d4e274eccfe8b2670
wirasenanurhakim-cloud <wirasena.nurhakim@gmail.com>
2026-06-11 09:19:33 +0700

    fix: remove large files

--- database/migrations/add_instagram_thumbnail.sql
+++
@@ -1,3 +0,0 @@
--- Add thumbnail caching columns to publish_instances
-ALTER TABLE publish_instances ADD COLUMN thumbnail_url TEXT;
-ALTER TABLE publish_instances ADD COLUMN last_thumbnail_update DATETIME;
